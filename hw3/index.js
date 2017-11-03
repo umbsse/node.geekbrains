7c7a48eebb5ba4ed18c0d62fd66544f8244ac0b1
umbsse <umbsse@gmail.com>
2017-11-03 15:18:31 +0300

    .each changed to map

--- hw3/index.js
+++ hw3/index.js
@@ -6,18 +6,17 @@ request('https://thehackernews.com/', function(err, res, body){
     if(err){console.log(err);}
     else{ 
     var $ = cheerio.load(body);
-    var posts = [];
-
-    $('.post.item.module').each(function(i, element){
+    
+    const posts = $('.post.item.module').map(function(i, element){
       
         var articleTitle = $('.url.entry-title.page-link',this);
         var articleText = $('.post-body.entry-content',this);
         
-        posts.push({
+        return {
             title: articleTitle.text(),
             href : articleTitle.attr('href'),
             text : articleText.text()
-        });
+        };
     });    
     
     console.log(posts);
